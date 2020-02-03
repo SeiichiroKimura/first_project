c23c3deed9666539f5d5bf4b16ccdd4447f93c0f
SeiichiroKimura <skim.lalala@gmail.com>
2020-02-03 19:41:57 +0900

    web site overview

--- lunchmap/urls.py
+++ lunchmap/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'lunchmap'
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('', views.index, name='index'),
     path('restaurant/', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('create/', views.CreateView.as_view(), name='create'),
--- lunchmap/views.py
+++ lunchmap/views.py
@@ -3,6 +3,16 @@ from .models import Category, Shop
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import PermissionDenied
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    context = {
+        'message': 'Welcome my BBS',
+        'players': ['勇者', '戦士', '魔法使い', '忍者']
+    }
+    return render(request, 'lunchmap/index.html', context)
 
 class IndexView(generic.ListView):
     model = Shop
