e19c84d17ced06f9de6948b711cf0a75b6fd78b4
SeiichiroKimura <skim.lalala@gmail.com>
2020-03-02 23:35:20 +0900

    full page updates

---
+++ lunchmap/static/js/layout.js
@@ -0,0 +1,21 @@
+// jQuery(function($) {
+//     $('.bg-slider').bgSwitcher({
+//         images: ['/static/lunchmap/images/welcome.jpg','/static/lunchmap/images/welcome02.jpg','/static/lunchmap/images/alohake.jpg'], // 切り替える背景画像を指定
+//     });
+// });
+
+// $('#timer').yycountdown({endDateTime:'2020/07/24 00:00:00'});
+//
+// console.log('test');
+//
+// $(document).ready(function(){
+// 　$("p").click(function(){
+// 　　$(this).text((new Date()).toLocaleString());
+//   });
+// });
+
+//東京オリンピックまでのカウントダウン
+    // $('#future_date').countdowntimer({
+    //     dateAndTime: "2020/07/24 00:00:00",
+    //     size: "lg"
+    // });
