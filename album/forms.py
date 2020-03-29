e945ad5ed1a99869aaff03c19056a1f5e6ae0ced
SeiichiroKimura <skim.lalala@gmail.com>
2020-03-29 22:09:03 +0900

    完成

--- album/forms.py
+++ album/forms.py
@@ -4,4 +4,4 @@ from .models import Image
 class ImageForm(forms.ModelForm):
     class Meta:
         model = Image
-        fields = ['picture', 'title']
+        fields = ['picture', 'comment']
--- album/models.py
+++ album/models.py
@@ -2,8 +2,7 @@ from django.db import models
 
 class Image(models.Model):
     picture = models.ImageField(upload_to='images/')
-    title = models.CharField(max_length=200)
+    comment = models.CharField(max_length=400, null=True)
 
     def __str__(self):
-        return self.title
-        
+        return self.comment
